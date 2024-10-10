482be8922077679bc16a412608299b678f1a8294
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-10 11:03:20 -0300

    Revendo os modificadores de acesso e entendendo a diferença entre o default e o protected

---
+++ todos-modificadores-acesso/src/br/com/empresa/projeto/modelo/ValorNegativoException.java
@@ -0,0 +1,7 @@
+package br.com.empresa.projeto.modelo;
+
+public class ValorNegativoException extends RuntimeException{
+    public ValorNegativoException(String message) {
+        super(message);
+    }
+}
